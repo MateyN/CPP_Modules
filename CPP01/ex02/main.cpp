fba2058fc841dc1d95a2af26c107c7fe567e46fe
Matey Nikolov <mateynnikolov@gmail.com>
2022-12-16 12:40:54 +0100

    ex02 done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 11:00:47 by mnikolov          #+#    #+#             */
+/*   Updated: 2022/12/15 11:09:51 by mnikolov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string str;
+    std::string *stringPTR;
+    std::string &stringREF = str;
+
+    str = "HI THIS IS BRAIN";
+    stringPTR = &str;
+
+    std::cout << "the address of str: " << &str << std::endl;
+    std::cout << "the address of stringPTR: " << &stringPTR << std::endl;
+    std::cout << "the address of stringREF: " << &stringREF << std::endl;
+
+    std::cout << "the value of str: " << str << std::endl;
+    std::cout << "the value of stringPTR: " << *stringPTR << std::endl;
+    std::cout << "the value of stringREF: " << stringREF << std::endl;
+}

24a59520d509ab549cdc07efbc5f8bb73c84c813
Matey Nikolov <mateynnikolov@gmail.com>
2022-12-29 14:17:41 +0100

    ex01 done

---
+++ CPP02/ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 12:23:44 by mnikolov          #+#    #+#             */
+/*   Updated: 2022/12/28 13:16:14 by mnikolov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void) 
+{
+Fixed       a;
+Fixed const b(10);
+Fixed const c(42.42f);
+Fixed const d(b);
+
+a = Fixed(1234.4321f);
+
+std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+std::cout << "c is " << c << std::endl;
+std::cout << "d is " << d << std::endl;
+
+std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+return 0;
+}

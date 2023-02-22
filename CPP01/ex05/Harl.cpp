34d48ec7f7c41ac011c9743fb3b3874004674753
Matey Nikolov <mateynnikolov@gmail.com>
2023-02-22 08:38:45 +0100

    done

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,12 +6,18 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 10:50:36 by mnikolov          #+#    #+#             */
-/*   Updated: 2022/12/22 12:49:25 by mnikolov         ###   ########.fr       */
+/*   Updated: 2023/02/22 08:38:19 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
-
+/*
+ Array "complains" of four function pointers that correspond to each of the recognized log levels.
+ Each function pointer points to a member function of the Harl class that handles the specified log level.
+ * loops through the lvls array and checks each string against the specified level.
+ If a match is found, the function uses the corresponding function pointer in the complains array
+ to call the appropriate member function on the current Harl instance.
+*/
 Harl:: Harl(void)
 {
     return ;
@@ -49,34 +55,33 @@ void    Harl:: error(void)
     std::cout << GREEN << "This is unacceptable! I want to speak to the manager immediately!" << RESET << std::endl;
     return ;
 }
-
-void    Harl:: complain(std::string level)
+void Harl::complain(std::string level)
 {
-    typedef void(Harl:: *fptr)(void);
-    
-    std::string lvls[4] = {
+    typedef void (Harl::*fptr)(void); // Define a function pointer type that takes no arguments and returns void
+
+    std::string lvls[4] = { // Define an array of strings representing log levels
         "DEBUG",
         "INFO",
         "WARNING",
         "ERROR"
     };
 
-    fptr    complains[4] = {
+    fptr complains[4] = { // Define an array of function pointers that correspond to each log level
         &Harl::debug,
         &Harl::info,
         &Harl::warning,
         &Harl::error
     };
-
+    
     int i = 0;
     while (i < 4)
     {
-        if (lvls[i] == level)
+        if (lvls[i] == level) // If the log level matches the specified level, call the corresponding log function
         {
-            (this->*complains[i])();
-            return ;
+            (this->*complains[i])(); // Use the function pointer to call the appropriate log function on the current Harl instance
+            return;
         }
         i++;
     }
-    return ;
+    return;
 }

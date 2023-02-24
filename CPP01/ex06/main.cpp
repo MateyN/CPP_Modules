72c4c2e097d689bab2b8b674de69258298961ff7
Matey Nikolov <mateynnikolov@gmail.com>
2023-02-24 13:29:58 +0100

    done

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 10:50:39 by mnikolov          #+#    #+#             */
-/*   Updated: 2023/02/23 10:31:06 by mnikolov         ###   ########.fr       */
+/*   Updated: 2023/02/24 13:29:16 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		std::cout << "Invalid number of arguments: Choose from DEBUG, INFO, WARNING and ERROR" << std::endl;
+		std::cout << RED << "Invalid number of arguments" << RESET << std::endl;
+		std::cout << "Choose from : " << GREEN << "DEBUG, INFO, WARNING and ERROR" << RESET << std::endl;
 		return -1;
 	}
 	_Harl.complain(av[1]); // calling the complain method of the _Harl instance with the second command-line argument.

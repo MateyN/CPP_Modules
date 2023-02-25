5306fdfbc35d8c335d8ae2b62eacdd85a13d6d1d
Matey Nikolov <mateynnikolov@gmail.com>
2023-02-25 11:18:19 +0100

    done

--- CPP01/ex04/Filestream.cpp
+++ CPP01/ex04/Filestream.cpp
@@ -31,11 +31,11 @@ int Filestream:: replaceFile(std::string fileName, std::string s1, std::string s
 {
     std::ifstream   ifs(fileName); // creates an ifstream object called ifs and opens the file with the name specified in the fileName parameter.
     std::string     newFile(fileName); // creates a new string called newFile and initializes it to be the same as the fileName parameter
-    std::string     buff; // appends the string ".replace" to the end of newFile
+    std::string     buff; // Used to store the contents of each line in the input file
 
     newFile += ".replace";
     std::ofstream   ofs(newFile); // creates an ofstream object called ofs and opens the new file
-    while (getline(ifs, buff))
+    while (getline(ifs, buff)) // iterate through each line of the input file and perform a string comparison and replacement operation on each line.
     {
         if (buff.compare(s1) == 0) // checks if the line contains the string specified in the s1 parameter.
             ofs << s2 << std::endl; // writes the replacement string specified in the s2 parameter to the output file
